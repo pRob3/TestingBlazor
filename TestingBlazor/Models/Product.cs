530eb6dfdda76f7d96cd28d580d99980b4cdf2fa
Dennis Asplind <skene@hotmail.com>
2024-09-16 09:06:29 +0200

    testing forms

--- TestingBlazor/Models/Product.cs
+++ TestingBlazor/Models/Product.cs
@@ -1,10 +1,30 @@
-﻿namespace TestingBlazor.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestingBlazor.Models;
 
 public class Product
 {
+    public Product()
+    {
+        AvailableAfter = DateOnly.FromDateTime(DateTime.Now);
+    }
+
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; } = string.Empty;
+    [Range(1, 1000)]
     public double Price { get; set; }
     public bool IsActive { get; set; }
     public IEnumerable<Product_Prop> ProductProperties { get; set; }
+
+    public Category Category { get; set; }
+    public DateOnly AvailableAfter { get; set; }
+
+}
+
+public enum Category
+{
+    Entree,
+    Appetizer,
+    Dessert
 }
